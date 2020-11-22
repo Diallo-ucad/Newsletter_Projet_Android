5c848d2853d912edb1209a460a81e3460e14d5c9
diallo <elhadj-mamadou-foula.diallo@etu.univ-cotedazur.fr>
2020-11-22 22:58:05 +0100

    Modification de la page d'acceuil

--- newsletter/src/main/java/com/mbds/newsletter/repository/AproposdenousDevelopperData.kt
+++ newsletter/src/main/java/com/mbds/newsletter/repository/AproposdenousDevelopperData.kt
@@ -7,7 +7,7 @@ object AproposdenousDevelopperData {
         Aproposdenous_devInfoItem(
             firstName = "DIALLO",
             lastName = "El.Mamadou Foula",
-            developper_git="@Diallo-ucad",
+            developper_git="pseudo-git: @Diallo-ucad",
             developper_img="https://lh3.googleusercontent.com/-PumFfAumiCM/XtWm4ucJjQI/AAAAAAAAANI/" +
                     "qR8UZPkyAooVlGHEFMktrqeHFWTxAZKPQCEwYBhgLKtQDAL1OcqyiCGYayrHF9JsgscPFrzHVop-NcYLk1QPPPYZ7UlG9f7" +
                     "L5dQyxywTAZgM6XigmGqGJdGZNxVqj85HAilZpGnPFE1zvmjjyP8XQR-OlngFORtnX-iX8uV95BnxihI6a5eHsXgd8zZcLRId4B6t9-hNI4TaZOJjIdh5wbTUQigRrVr5jwwGsgISP9iNZLqs9NKzzMBNCeJFDZxVtf9IyaciTDT9U0InqLMae8NPY4806aVXSh1rvSa0Lk0B4kaKbJHH9zGj8-ghK_fheeAV0iQnghrAKPTbhdHzisIWjWZc8xm7L2bmWH62utY10ueCDl_OJsrAq1QT-vd6pbUKSsGbPMNJcgptJSeNwx_DAl5y3DLCFWp6YvazDZjO11lb53Q1Zb8z9StfMRm6LqiSP3st-1pNLb8iLplQG0dWUm0w4HkYv923J5_ZBmULIzjY0pz5nb3Dq7mrxiRgGjUK7xPe7J-Juq7GJV0nsCS14PP9lgIcDAAn1z7igunOSkUQePjAWvE_1VHa1C1LzuAA66uKYAR736oLWnbTyft3GLy0EMu-koHVPghrnxMl_DUbCedF90o_hTc7G96n44gQVDDOiZNuuikUzKR2R9a7EnxEPMNLPoP0F/w140-h140-p/thumbnail_IMG_2172.jpg"
@@ -15,15 +15,14 @@ object AproposdenousDevelopperData {
             Aproposdenous_devInfoItem(
                     firstName= "ABDALLAH",
                     lastName = "SWAHABADINE",
-                    developper_git="@Swabahadine",
+                    developper_git="pseudo-git: @Swabahadine",
                 developper_img="https://media-exp1.licdn.com/dms/image/C4D03AQHcNLjx0cKP_A/profile-displayphoto-shrink_400_400/0?e=1610582400&v=beta&t=DhQVcFi3GwEZnapZ7WVn9Rl4Mz0yc5mlxl2Slz6uTQg"
             ),
             Aproposdenous_devInfoItem(
                     firstName = "NITEKA",
                     lastName= "Lys Ciella",
-                    developper_git="@Niteka2",
+                    developper_git="pseudo-git: @Niteka2",
                     developper_img="https://scontent-mrs2-2.xx.fbcdn.net/v/t1.0-9/90368550_2777150312362175_4090101105487249408_n.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=ZdJVgr-NQCcAX8GLp5j&_nc_ht=scontent-mrs2-2.xx&oh=5225f8486e3e3cc2a107d254e4d5bbe5&oe=5FCFFDEF"
-
             )
     )
 }
